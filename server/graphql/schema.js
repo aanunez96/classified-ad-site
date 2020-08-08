b5ffcf94f89ebddbd12a29ef9116a6536db8e86a
adrian alberto nunez capote <aanunez@estudiantes.uci.cu>
2020-08-08 12:00:51 -0400

    define schema graphql

--- server/graphql/schema.js
+++ server/graphql/schema.js
@@ -2,11 +2,60 @@ const {gql} = require('apollo-server-express');
 
 const typeDefs = gql`
   type Query {
-    ad(adId: ID): Ad
-  }  
+    ads(userId: ID, classification: Category, recently: Boolean): [Ad]
+    ad(adId: ID!): Ad
+    categories : [Category]
+    user(userId: ID): [User]
+  }
+    
   type Ad{
     _id: ID!
-    description: String!
+    description: String
+    tittle: String!
+    owner: User!
+    classification: Category
+    price: Int!
+    date:  String!
   }
+  
+  enum Category{
+    car
+    motorcycle
+    property
+    cycle
+    clothing
+    watch
+    gadget
+    mobile
+  }
+  
+  type Profile{
+     name: String
+     lastName: String
+     avatar: String
+     number: Int
+  }
+  
+  extend type User {
+  _id: ID
+  profile: Profile
+  }
+  
+  extend input CreateUserInput {
+      profile: CreateUserProfileInput!
+  }
+  
+  input CreateUserProfileInput {
+     name: String
+     lastName: String
+     avatar: String
+     number: Int
+  }
+  
+  type Mutation{
+    createAd(tittle: String!, owner: ID!, description: String, classification: Category!, price: Int!, date: String!):ID
+    
+    modifyAd(tittle: String, adId: ID!, description: String, classification: Category, price: Int, date: String): ID
+  } 
 `;
 module.exports = typeDefs;
